11a4d98c893b0f22e51d217f718ec92a12460315
Vitor Sampaio <vitorsampaio.tds2.senai@gmail.com>
2025-04-10 14:21:58 -0300

    feat: create course routes and server.js

---
+++ src/routes/courseRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import courseController from "../controllers/courseController.js";
+const router = express.Router();
+router.get("/", courseController.getAll);
+router.post("/", courseController.create);
+router.get("/:id", courseController.getById);
+router.put("/:id", courseController.update);
+router.delete("/:id", courseController.delete);
+export default router;
--- src/routes/tarefaRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import tarefaController from "../controllers/tarefaController.js";
-const router = express.Router();
-router.get("/", tarefaController.getAll);
-router.post("/", tarefaController.create);
-router.get("/:id", tarefaController.getById);
-router.put("/:id", tarefaController.update);
-router.delete("/:id", tarefaController.delete);
-export default router;
